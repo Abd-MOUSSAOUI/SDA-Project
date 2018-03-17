c006508fb0d82f83af7062a0843b2aff420bcff8
Nidhal DOGGA <dogga.nidhal@gmail.com>
2018-03-17 12:12:48 +0100

    akefile

--- include/ordered_set.h
+++ include/ordered_set.h
@@ -8,9 +8,17 @@
 
 typedef struct ordered_set_s
 {
-    
+
 } ordered_set_t;
 
+typedef int (*ordered_set_cmp)(const void *);
 
+ordered_set_t *ordered_set_create(void);
+void ordered_set_destroy(ordered_set_t **set);
+size_t ordered_set_get_count(const ordered_set_t *set);
+void ordered_set_insert(ordered_set_t *set, void *value);
+int ordered_set_contains(const ordered_set_t *set, const void *value, ordered_set_cmp cmp_func);
+void ordered_set_print(const ordered_set_t *set);
+ordered_set_t *ordered_set_intersect(const ordered_set_t **sets);
 
 #endif

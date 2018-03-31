c55408a8b66f74dc81094c064c6d608737b0428f
Nidhal DOGGA <dogga.nidhal@gmail.com>
2018-03-31 05:23:56 +0200

    compiler patch

--- unit-tests/tests.c
+++ unit-tests/tests.c
@@ -4,14 +4,26 @@
 #include <stdlib.h>
 #include "analyzer.h"
 
+#ifndef __clang__
+void print_word(const char *word)
+{
+    printf("%s:", word);
+}
+#endif
+
 int main(int argc, char *argv[])
 {
 
     const char *testString = "Hello world this is a testing string";
 
+    #ifdef __clang__
     str_split(testString, ' ', ^(const char *word){
         printf("%s:", word);
     });
+    #else
+    str_split(testString, ' ', print_word);
+    #endif
+    
     putchar('\n');
 
     return EXIT_SUCCESS;

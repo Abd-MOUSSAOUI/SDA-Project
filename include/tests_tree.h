3e6dda6ca2608e57ab8d89ae7c9d740ae95e874e
Abd-MOUSSAOUI <a.moussaoui05@gmail.com>
2018-04-02 00:06:28 +0200

    getHeight

--- include/tests_tree.h
+++ include/tests_tree.h
@@ -7,10 +7,13 @@
 #include <math.h>
 #include "bst.h"
 #define bool int
+#define true 1
+#define false 0
 
 
 bool tree_isBalanced(bst_t *t);
+int tree_getHeight(bst_t *t);
 int max(int a, int b);
-int height(bst_t *t);
+
 
 #endif
--- src/tests_tree.c
+++ src/tests_tree.c
@@ -8,15 +8,15 @@ bool tree_isBalanced(bst_t *t)
    int lh;
    int rh;
 
-   if(t == NULL) return 1;
+   if(t == NULL) return true;
 
-   lh = height(t->left_child);
-   rh = height(t->right_child);
+   lh = tree_getHeight(t->left_child);
+   rh = tree_getHeight(t->right_child);
 
    if( abs(lh-rh) <= 1 && tree_isBalanced(t->left_child)
-                       && tree_isBalanced(t->right_child)) return 1;
+                       && tree_isBalanced(t->right_child)) return true;
 
-   return 0;
+   return false;
 }
 
 
@@ -26,8 +26,8 @@ int max (int X, int Y)
 }
 
 
-int height(bst_t *t)
+int tree_getHeight(bst_t *t)
 {
    if(t == NULL) return 0;
-   return 1 + max(height(t->left_child), height(t->right_child));
+   return 1 + max(tree_getHeight(t->left_child), tree_getHeight(t->right_child));
 }
